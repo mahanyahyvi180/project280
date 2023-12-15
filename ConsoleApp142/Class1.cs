0990612e9d6d1090b982e2c70c2bb7206e699d95
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-12-15 10:25:31 +0330

    Add files via upload

---
+++ ConsoleApp142/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp142
+{
+   public class NegativeNumberException:Exception
+    {
+        public NegativeNumberException() : base("Illegal operation for a negative number")
+        {
+            //empty body
+        }
+
+        public NegativeNumberException(string messageValue) : base(messageValue)
+        {
+            //empty body
+        }
+
+        public NegativeNumberException(string messageValue, Exception inner) : base(messageValue, inner)
+        {
+            //empty body
+        }
+    }
+}
---
+++ ConsoleApp142/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var continueLoop = true;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter a value to calculate the square root of:");
+                    double inputValue = double.Parse(Console.ReadLine());
+                    double result = SquareRoot(inputValue);
+
+                    Console.WriteLine($"The square root of {inputValue} is{result:F6}\n");
+                    continueLoop = false;
+                }
+                catch (FormatException formatException)
+                {
+                    Console.WriteLine("\n" + formatException.Message);
+                    Console.WriteLine("please enter a double value.\n");
+                }
+                catch (NegativeNumberException negativeNumberException)
+                {
+                    Console.WriteLine("\n" + negativeNumberException.Message);
+                    Console.WriteLine("please enter a non-negative value.\n");
+                }
+            } while (continueLoop);
+            Console.ReadLine();
+                   
+        }
+        public static double SquareRoot(double value)
+        {
+            if (value < 0)
+            {
+                throw new NegativeNumberException("Square root of negative number not permitted");
+            }
+            else
+            {
+                return Math.Sqrt(value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
